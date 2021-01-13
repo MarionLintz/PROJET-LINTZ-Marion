506be4e921b8f01e81dede076de84126f62e2a5c
Marion Lintz <marionnirina@gmail.com>
2021-01-13 20:23:13 +0100

    cors 401

--- back/public/index.php
+++ back/public/index.php
@@ -24,6 +24,13 @@ $options = [
     "path" => ["/user", "/products"],
     "ignore" => ["/user/ping", "/user/login", "/user/register"],
     "error" => function ($response, $arguments) {
+        $response = $response
+            ->withHeader('Access-Control-Allow-Origin', 'https://cnam-marionl-projet-front.herokuapp.com')
+            ->withHeader('Access-Control-Allow-Headers', '*')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+            ->withHeader('Access-Control-Allow-Credentials', 'true')
+            ->withHeader('Access-Control-Expose-Headers', '*');
+            
         $data = array('ERREUR' => 'Connexion', 'ERREUR' => 'JWT Non valide');
         $response = $response->withStatus(401);
         return $response->withHeader("Content-Type", "application/json")->getBody()->write(json_encode($data));
